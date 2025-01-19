cb63317993800c21df4ef1ac2aadf1c9968e0589
imsomena <italocomunica@gmail.com>
2025-01-19 14:33:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var leonardo = window.document.getElementById("leonardo")
+var saman = window.document.getElementById("saman")
+var bruna = window.document.getElementById("bruna")
+
+function RolarParaDireita(){
+    leonardo.style ="display:nome"
+    bruna.style ="display:flex"
+
+}
